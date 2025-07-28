5fb6c39cd3cd11391258ec9632a09244dc5f62a5
endersonjack <endersonjackson2@gmail.com>
2025-07-28 10:30:16 -0300

    fix Key

--- gser/settings.py
+++ gser/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"],
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
