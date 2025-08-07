0f25e9754f319624ff5a93bbdf77df9506706b8b
endersonjack <endersonjackson2@gmail.com>
2025-08-07 14:52:55 -0300

    fix url media Debug

--- gser/urls.py
+++ gser/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from contratos.views import *
@@ -53,6 +55,11 @@ urlpatterns = [
 
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
